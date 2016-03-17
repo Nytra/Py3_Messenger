7e024836d8ad5a236ae17794459ec84f2f389c48
Nytra <samueltscott@gmail.com>
2016-03-17 18:00:14 +0000

    Update updater.py

--- updater.py
+++ updater.py
@@ -5,7 +5,6 @@ r = request.urlopen("https://github.com/Nytra/messenger/archive/master.zip")
 print("Extracting...")
 with zipfile.ZipFile(io.BytesIO(r.read())) as z:
     z.extractall()
-time.sleep(2)
 print("Moving files...")
 cmd = "move \"{}\\messenger-master\\*\" \"{}\"".format(os.path.abspath(""), os.path.abspath(""))
 os.system(cmd)
