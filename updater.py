0d7a1b0b36044ab97e91c63f012e8465edec6082
Nytra <samueltscott@gmail.com>
2016-03-17 15:38:25 +0000

    Delete updater.py

--- updater.py
+++
@@ -1,6 +0,0 @@
-from urllib import request
-
-response = request.urlopen('https://github.com/Nytra/messenger/archive/master.zip')
-data = response.read()
-with open("master.zip", 'w') as f:
-    f.write(data)
