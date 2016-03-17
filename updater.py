38bedc6a657500aa37291baa18cf8b03724003f0
Nytra <samueltscott@gmail.com>
2016-03-17 16:45:57 +0000

    Fixed

--- updater.py
+++ updater.py
@@ -5,13 +5,13 @@ r = request.urlopen("https://github.com/Nytra/messenger/archive/master.zip")
 print("Extracting...")
 with zipfile.ZipFile(io.BytesIO(r.read())) as z:
     z.extractall()
-files = ["updater.py", "messenger.py", "server.py"]
+files = ["updater.py", "messenger.py", "server.py", "README.md"]
 time.sleep(2)
 print("Moving files...")
 for file in files:
     cmd = "move \"{}\\messenger-master\\{}\" \"{}\"".format(os.path.abspath(""), file, os.path.abspath(""))
     os.system(cmd)
-cmd = r"rmdir /Q /S {}\messenger-master".format(os.path.abspath(""))
+cmd = "rmdir \"{}\\messenger-master\"".format(os.path.abspath(""))
 print("Cleaning up...")
 os.system(cmd)
 
