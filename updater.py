6e85217f45e2b6b7ea361488fc615d059c36f43a
Nytra <samueltscott@gmail.com>
2016-03-17 15:46:27 +0000

    Added visual feedback

--- updater.py
+++ updater.py
@@ -1,5 +1,7 @@
 from urllib import request
 import zipfile, io
+print("Extracting file...")
 r = request.urlopen("https://github.com/Nytra/messenger/archive/master.zip")
 with zipfile.ZipFile(io.BytesIO(r.read())) as z:
     z.extractall()
+print("Done.")
