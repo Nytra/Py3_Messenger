c85b1d4d45a8cfe42338c336c1e806b1514b4382
Nytra <samueltscott@gmail.com>
2016-03-17 16:42:06 +0000

    Update updater.py

--- updater.py
+++ updater.py
@@ -1,19 +1,18 @@
 from urllib import request
 import zipfile, io, os, time
-print("Extracting file...")
+print("Downloading master zip file...")
 r = request.urlopen("https://github.com/Nytra/messenger/archive/master.zip")
+print("Extracting...")
 with zipfile.ZipFile(io.BytesIO(r.read())) as z:
     z.extractall()
 files = ["updater.py", "messenger.py", "server.py"]
-print(os.path.abspath(""))
 time.sleep(2)
+print("Moving files...")
 for file in files:
     cmd = "move \"{}\\messenger-master\\{}\" \"{}\"".format(os.path.abspath(""), file, os.path.abspath(""))
-    print(cmd)
-    input()
     os.system(cmd)
 cmd = r"rmdir /Q /S {}\messenger-master".format(os.path.abspath(""))
-print(cmd)
+print("Cleaning up...")
 os.system(cmd)
 
 print("Done.")
