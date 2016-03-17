504decf4198fc6fc47753a25f4f4d9075e2a471b
Nytra <samueltscott@gmail.com>
2016-03-17 16:35:43 +0000

    Delete updater.py

--- updater.py
+++
@@ -1,7 +0,0 @@
-from urllib import request
-import zipfile, io
-print("Extracting file...")
-r = request.urlopen("https://github.com/Nytra/messenger/blob/master/master.zip?raw=true")
-with zipfile.ZipFile(io.BytesIO(r.read())) as z:
-    z.extractall()
-print("Done.")
