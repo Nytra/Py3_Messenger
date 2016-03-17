9438c8dab345e81b0901361a30797f0a009f6a8a
Nytra <samueltscott@gmail.com>
2016-03-17 15:52:25 +0000

    Fixed zip url

--- updater.py
+++ updater.py
@@ -1,7 +1,7 @@
 from urllib import request
 import zipfile, io
 print("Extracting file...")
-r = request.urlopen("https://github.com/Nytra/messenger/archive/master.zip")
+r = request.urlopen("https://github.com/Nytra/messenger/blob/master/master.zip?raw=true")
 with zipfile.ZipFile(io.BytesIO(r.read())) as z:
     z.extractall()
 print("Done.")
